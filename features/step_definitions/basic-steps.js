b48e6f21612ddabc7c70de36aee0875cc70d32e7
pedro_bras <pedro.c.bras@gmail.com>
2019-07-16 12:43:33 +0200

    setting up helper functions on worldjs, first test visit the landing page going green

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,8 +1,11 @@
-const { Given, Then } = require("cucumber");
+const { After, Given, Then, When } = require('cucumber')
 
-Given("I visit the site", function() {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
+After(async function() {
+    return await this.closeHomePage()
+  })
+
+Given("I visit the site", async function() {
+    return await this.openHomePage()
 });
 
 Then("I should see {string}", function(string) {
