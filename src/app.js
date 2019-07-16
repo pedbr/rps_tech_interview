c9aeb99b825bf447025c8e076368798f4ef0f710
pedro_bras <pedro.c.bras@gmail.com>
2019-07-16 18:17:11 +0200

    refactors user choice funcitons so that result can be a variable

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const playRock = () => {
+    let playerChoice = `ROCK`
     let displayChoice = document.getElementById("display-choice")
-    displayChoice.innerHTML = `YOU PICKED ROCK!`
+    displayChoice.innerHTML = `YOU PICKED ${playerChoice}!`
 }
 
 const playPaper = () => {
+    let playerChoice = `PAPER`
     let displayChoice = document.getElementById("display-choice")
-    displayChoice.innerHTML = `YOU PICKED PAPER!`
+    displayChoice.innerHTML = `YOU PICKED ${playerChoice}!`
 }
 
 const playScissors = () => {
+    let playerChoice = `SCISSORS`
     let displayChoice = document.getElementById("display-choice")
-    displayChoice.innerHTML = `YOU PICKED SCISSORS!`
+    displayChoice.innerHTML = `YOU PICKED ${playerChoice}!`
 }
 
 document.getElementById("play-rock").addEventListener("click", playRock);
