e8f3ec3d799d473247aa3f781ad67b84126a4386
pedro_bras <pedro.c.bras@gmail.com>
2019-07-16 22:42:57 +0200

    adds if statement to know game result

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const getComputerChoice = () => {
     let computerPlay = Math.random();
     if (computerPlay < 0.34) {
-        computerPlay = "ROCK";
+        computerPlay = `ROCK`;
     } else if(computerPlay <= 0.67) {
-        computerPlay = "PAPER";
+        computerPlay = `PAPER`;
     } else {
-        computerPlay = "SCISSORS";
+        computerPlay = `SCISSORS`;
     }
     return computerPlay
 }
@@ -13,22 +13,46 @@ const getComputerChoice = () => {
 const playRock = () => {
     let playerChoice = `ROCK`
     let computerChoice = getComputerChoice()
+    let showResult
+    if (computerChoice == `ROCK`){
+        showResult = `IT'S A TIE!`
+    } else if (computerChoice == `PAPER`){
+        showResult = `YOU LOSE!`
+    } else {
+        showResult = `YOU WIN!`
+    }
     let displayChoice = document.getElementById("display-choice")
-    displayChoice.innerHTML = `YOU PICKED ${playerChoice} AND COMPUTER CHOSE ${computerChoice}!`
+    displayChoice.innerHTML = `YOU PICKED ${playerChoice} AND COMPUTER CHOSE ${computerChoice}! ${showResult}`
 }
 
 const playPaper = () => {
     let playerChoice = `PAPER`
     let computerChoice = getComputerChoice()
+    let showResult
+    if (computerChoice == `ROCK`){
+        showResult = `YOU WIN!`
+    } else if (computerChoice == `PAPER`){
+        showResult = `IT'S A TIE!`
+    } else {
+        showResult = `YOU LOSE!`
+    }
     let displayChoice = document.getElementById("display-choice")
-    displayChoice.innerHTML = `YOU PICKED ${playerChoice} AND COMPUTER CHOSE ${computerChoice}!`
+    displayChoice.innerHTML = `YOU PICKED ${playerChoice} AND COMPUTER CHOSE ${computerChoice}! ${showResult}`
 }
 
 const playScissors = () => {
     let playerChoice = `SCISSORS`
     let computerChoice = getComputerChoice()
+    let showResult
+    if (computerChoice == `ROCK`){
+        showResult = `YOU LOSE!`
+    } else if (computerChoice == `PAPER`){
+        showResult = `YOU WIN!`
+    } else {
+        showResult = `IT'S A TIE!`
+    }
     let displayChoice = document.getElementById("display-choice")
-    displayChoice.innerHTML = `YOU PICKED ${playerChoice} AND COMPUTER CHOSE ${computerChoice}!`
+    displayChoice.innerHTML = `YOU PICKED ${playerChoice} AND COMPUTER CHOSE ${computerChoice}! ${showResult}`
 }
 
 document.getElementById("play-rock").addEventListener("click", playRock);
