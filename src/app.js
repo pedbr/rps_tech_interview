a08927b7b6db70c406769800c20c38e4978b808e
pedro_bras <pedro.c.bras@gmail.com>
2019-07-16 18:29:49 +0200

    builds random computer choice function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
+const getComputerChoice = () => {
+    let computerPlay = Math.random();
+    if (computerPlay < 0.34) {
+        computerPlay = "ROCK";
+    } else if(computerPlay <= 0.67) {
+        computerPlay = "PAPER";
+    } else {
+        computerPlay = "SCISSORS";
+    }
+    return computerPlay
+}
+
 const playRock = () => {
     let playerChoice = `ROCK`
+    let computerChoice = getComputerChoice()
     let displayChoice = document.getElementById("display-choice")
-    displayChoice.innerHTML = `YOU PICKED ${playerChoice}!`
+    displayChoice.innerHTML = `YOU PICKED ${playerChoice} AND COMPUTER CHOSE ${computerChoice}!`
 }
 
 const playPaper = () => {
